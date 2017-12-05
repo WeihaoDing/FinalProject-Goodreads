0179a355e86479c68a4878022274a83bfd4a5c62
yg82@uw.edu <valeswara-raogazula@D-69-91-178-135.dhcp4.washington.edu>
2017-12-05 15:22:51 -0800

    Fixes #11

---
+++ review.R
@@ -0,0 +1,97 @@
+library("jsonlite")
+library("httr")
+library("tidytext")
+library("dplyr")
+library("stringr")
+library("rvest")
+library("wordcloud")
+
+source("./api-key.R")
+# test example: reviewdata <- ReviewWords("978-0375842207", "The Book Thief")
+
+# Get all words in the book review
+ReviewWords <- function(book.isbn, book.title) {
+  base.url <- 'http://api.nytimes.com/svc/books/v3/reviews.json'
+  query.params <- list(isbn = book.isbn, title = book.title,  api_key = yogasaikey)
+  response <- GET(base.url, query = query.params)
+  body <- content(response, "text")
+  results <- fromJSON(body)
+  book.reviews <- flatten(results$results)
+  
+  url <- book.reviews$url[1]
+  page <- read_html(url)
+  
+  # Extract review text from webpage
+  review.content <- page %>% html_nodes('.story-body-text') %>% html_text() 
+  
+  # Create review text dataframe 
+  dfcontent <- data.frame(review.content, stringsAsFactors = FALSE)
+  
+  # Create tidytext structure of all words in review
+  all.words <- dfcontent %>% unnest_tokens(word, review.content)
+
+  # Get all word counts
+  word.count <- all.words %>% 
+    group_by(word) %>% 
+    summarize(count = n()) %>% 
+    arrange(-count)
+
+  # Remove stop words
+  content.words <- word.count %>% 
+    anti_join(stop_words, by="word")
+  
+  # Get content word counts -- arrange content.words by descending count
+  content.count <- content.words %>%
+    arrange(-count)
+  
+  return(content.count)
+}
+
+reviewdata <- ReviewWords <- function(book.isbn, book.title)
+
+# Get wordcloud with negative words in review
+NegativeCloud <- function(reviewdata) {
+  
+  # Get negative words in "nrc" Lexicon
+  nrc.neg <- get_sentiments("nrc") %>% 
+    filter(sentiment == "negative")
+
+  # Count the negative words in the review data
+  neg.words <- reviewdata %>%
+    inner_join(nrc.neg, by ="word") 
+
+  # Create the negative word cloud
+  ncloud <- wordcloud(neg.words$word, neg.words$count, scale = c(3,.5), min.freq = 1, max.words = Inf, random.order = TRUE, colors = "#C40500")
+  
+  return(ncloud)
+}
+
+negative.cloud <- NegativeCloud(reviewdata)
+
+# Get wordcloud with positive words in review
+PositiveCloud <- function(content.count) {
+  
+  # Get positive words in "nrc" Lexicon
+  nrc.pos <- get_sentiments("nrc") %>% 
+    filter(sentiment == "positive")
+  
+  # Count the positive words in the review data
+  pos.words <- content.count %>%
+    inner_join(nrc.pos, by ="word") 
+  
+  # Create the positive word cloud
+  pcloud <- wordcloud(pos.words$word, pos.words$count, scale = c(3,.5), min.freq = 1, max.words = Inf, 
+                      random.order = TRUE, colors = "#00BFC4")
+  return(pcloud)
+}
+
+positive.cloud <- PositiveCloud(reviewdata)
+
+
+#this source: http://tidytextmining.com/tidytext.html 
+#provided helpful information for working with tidytext data and
+#for text mining in R
+
+
+
+
